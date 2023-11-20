1a047480bf198b5878a3e317ff7988e475cf2657
Samuel Gómez Pérez <samgope100@gmail.com>
2023-11-20 10:09:51 +0100

    Cambio Sam
    Cambio Sam script

---
+++ Assets/Scriptsam.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scriptsam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Esto es el Start
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
