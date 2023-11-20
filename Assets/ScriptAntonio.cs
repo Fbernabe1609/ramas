c97b842969755642b24bf140c9273e966a314ec8
MrAntonio7 <antonioalvarezmalagon@gmail.com>
2023-11-20 10:04:19 +0100

    Cambios Antonio
    Cambios hechos en la rama principal de Antonio, script creado

---
+++ Assets/ScriptAntonio.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptAntonio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola soy Antonio");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
